1912f73d89abe6075175a49db93ed80620ecb431
uf5 <54855684+uf5@users.noreply.github.com>
2022-05-07 11:06:00 +0500

    Use readline for user input

--- app/Main.hs
+++ app/Main.hs
@@ -6,22 +6,24 @@ import Lib
 import System.Environment (getArgs)
 import System.IO
 
+import System.Console.Haskeline
+import Control.Monad.IO.Class (liftIO)
+
 main :: IO ()
-main = do
-  args <- getArgs
-  let repl = do
-        putStr "trans> "
-        hFlush stdout
-        s <- getLine
-        case s of
-          "" -> return ()
-          _ -> do
-            let parsed = readScript s
-            print parsed
-            let expanded = expandModule parsed
-            print expanded
-            let translated = transModule expanded
-            print translated
-            I.writeFile "out.json" (encodeToLazyText translated)
-            repl
-  repl
+main = runInputT defaultSettings loop
+   where
+       loop :: InputT IO ()
+       loop = do
+           minput <- getInputLine "% "
+           case minput of
+               Nothing -> return ()
+               Just "" -> return ()
+               Just s -> do
+                 let parsed = readScript s
+                 outputStrLn $ show parsed
+                 let expanded = expandModule parsed
+                 outputStrLn $ show expanded
+                 let translated = transModule expanded
+                 outputStrLn $ show translated
+                 liftIO $ I.writeFile "out.json" (encodeToLazyText translated)
+                 loop
