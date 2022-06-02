b947fd146fb7bf9b8db34a040faf6650b84653ae
uf5 <notbailey13@gmail.com>
2022-06-02 14:42:23 +0500

    Use `HashMap`

--- src/Language/Translucent/Trans.hs
+++ src/Language/Translucent/Trans.hs
@@ -1,34 +1,37 @@
 module Language.Translucent.Trans (trans, transModule) where
 
 import Control.Monad.Writer
+import Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HashMap
 import Data.Text (pack)
 import Language.Translucent.PythonAst as P
 import Language.Translucent.Result
 import Language.Translucent.Types as T
 
-forms :: [(String, [LispVal] -> Result)]
+forms :: HashMap String ([LispVal] -> Result)
 forms =
-  [ ( "do",
-      foldl1 (+++) . map trans
-    ),
-    ( "if",
-      ( \[x, y, z] -> do
-          cond <- sub x
-          tell [If cond (block y) (block z)]
-          return $ Constant P.None
+  HashMap.fromList
+    [ ( "do",
+        foldl1 (+++) . map trans
+      ),
+      ( "if",
+        ( \[x, y, z] -> do
+            cond <- sub x
+            tell [If cond (block y) (block z)]
+            return $ Constant P.None
+        )
+          . map trans
+      ),
+      -- TODO
+      ( "=",
+        ( \[a, b] -> do
+            a <- sub a
+            b <- sub b
+            return $ Compare a [Eq] [b]
+        )
+          . map trans
       )
-        . map trans
-    ),
-    -- TODO
-    ( "=",
-      ( \[a, b] -> do
-          a <- sub a
-          b <- sub b
-          return $ Compare a [Eq] [b]
-      )
-        . map trans
-    )
-  ]
+    ]
 
 trans :: LispVal -> Result
 trans T.None = return $ Constant P.None
@@ -45,7 +48,7 @@ trans (T.SExp (h : t)) = case lookupForm of
     return $ Call fn args []
   where
     lookupForm = case h of
-      (T.Symbol x) -> lookup x forms
+      (T.Symbol x) -> HashMap.lookup x forms
       _ -> Nothing
 trans x = error $ "unknown expression: " ++ show x
 
