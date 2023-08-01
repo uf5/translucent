93b5c841d36dfe34a8a1b710e829cddbd97c948a
uf5 <notbailey13@gmail.com>
2023-08-01 19:35:34 +0500

    Write parsing errors to stderr

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,11 @@
 module Main where
 
+import Data.Text.IO (hPutStrLn)
 import Parser
 
 main :: IO ()
-main = getLine >>= putTextLn . handleEither . parse
-  where
-    handleEither (Left x) = x
-    handleEither (Right x) = show x
+main = do
+  prog <- getLine
+  case parse prog of
+    (Left x) -> hPutStrLn stderr x
+    (Right x) -> putTextLn (show x)
